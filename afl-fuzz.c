2a2a1a86ed74ac7b04a78df72742e158b52673fa
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2017-08-25 15:54:35 -0400

    Add havoc n mode fix

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -5934,10 +5934,10 @@ havoc_stage:
 
     u32 use_stacking = 1 << (1 + UR(HAVOC_STACK_POW2));
     // SIDD START
-    if (havoc_n_mode == -2) {
+    if (havoc_n_mode == 21) {
         use_stacking = 1 << (1 + UR(5));
     }
-    else if (havoc_n_mode == -1) {
+    else if (havoc_n_mode == 22) {
         use_stacking = 1 << (6 + UR(5));
     }
     else if (havoc_n_mode == 1) {
@@ -7596,8 +7596,7 @@ int main(int argc, char** argv) {
       // SIDD START
       case 'a':
         SAYF("Entering case 'a'!\n");
-        fprintf(optarg);
-        if (sscanf(optarg, "%d", &havoc_n_mode) < 1)
+        if (sscanf(optarg, "%u", &havoc_n_mode) < 1)
             FATAL("Bad syntax used for -a");
       break;
       // SIDD END
